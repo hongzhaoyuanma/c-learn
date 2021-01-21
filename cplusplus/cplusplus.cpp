d888e009402a2c0d30d8cf430b55e8d89fe124f3
老张学技术 <1498814665@qq.com>
2021-01-21 21:54:49 +0800

    module

--- cplusplus/cplusplus.cpp
+++ cplusplus/cplusplus.cpp
@@ -1,9 +1,17 @@
 ﻿#pragma once
 #include <iostream>
+export module helloworld;  // module declaration
+import <iostream>;         // import declaration
 
+export void hello() {      // export declaration
+    std::cout << "Hello world!\n" << std::endl;
+}
+
+import helloworld;
 int main()
 {
-    std::cout << "Hello World!\n" <<std::endl;
+    //std::cout << "Hello World!" <<std::endl;
+    hello();
     return 0;
 }
 
